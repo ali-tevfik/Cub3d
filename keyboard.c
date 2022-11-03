427e6d035740c99a232c985e7aae7f442da0cfa9
ali-tevfik <tevfikdoner@hotmail.com>
2022-11-03 22:32:16 +0100

    ray added

--- keyboard.c
+++ keyboard.c
@@ -1,54 +1,66 @@
 #include "cub3d.h"
 
-void draw_line(t_vars *data, double angle, int line_lenght, long color)
+
+void	move(t_vars *vars, int keycode)
 {
-    int x1;
-    int y1;
-
-    for(int i = 0; i < line_lenght; i += 1)
-    {
-        x1 = i * sin(angle) + data->player.x;
-        y1 = i * cos(angle) + data->player.y;
-        if (x1 < 15 * 50 && x1 > 0  && y1 < 6 * 50 && y1 > 0)
-        {
-			my_mlx_pixel_put(&data->player, x1, y1, color);
-    		printf("draw line x value = %d, y value = %d\n", x1, y1);
-		}
-    }
-	// mlx_put_image_to_window(data->mlx, data->win,
-	// 	data->player.img_ptr, x1, y1);
-}
-
-
-
-void	change_position(t_vars *vars, int keycode)
-{
-	maps_load(vars, 1);
+	clean_maps(vars);
 	if (keycode == LEFT || keycode == A)
 	{
-		//vars->player.x = vars->player.x - vars->game_speed;
-		vars->player.x = vars->player.x - 15;
-		vars->player.y = vars->player.y - 15;
+		printf("\n LEFT\n--------------------\n");
+		vars->player.pa -= 0.1 * M_PI; 
+		printf("angle = %f\n", vars->player.pa / (M_PI));
+		if (vars->player.pa < 0)
+			vars->player.pa += 2 * M_PI;
+		printf("angle after  = %f\n", vars->player.pa / (M_PI));
+			vars->player.d_x = cos(vars->player.pa) * 5;
+			vars->player.d_y = sin(vars->player.pa) * 5;
+		printf("_d %f %f\n",vars->player.d_x, vars->player.d_y);
+		printf("left x %d  y %d\n",vars->player.x, vars->player.y);
 	}
+
 	if (keycode == RIGHT || keycode == D)
-		vars->player.x = vars->player.x + vars->game_speed;
+	{
+		printf("\n RIGHT\n--------------------\n");
+		printf("angle before %f\n",vars->player.pa  / (M_PI));
+		vars->player.pa+=0.1 * M_PI; 
+		if (vars->player.pa > 2 * M_PI )
+			vars->player.pa -= 2 * M_PI;
+		vars->player.d_x =  cos(vars->player.pa) * 5;
+		vars->player.d_y = sin(vars->player.pa) * 5;
+		printf("angle = %f\n", vars->player.pa / (M_PI));
+		printf("_d %f %f\n",vars->player.d_x, vars->player.d_y);
+		printf("right %d %d\n",vars->player.x, vars->player.y);
+		
+	}
 	if (keycode == DOWN || keycode == S)
-		vars->player.y = vars->player.y + vars->game_speed;
+	{
+		printf("\n DOWN\n--------------------\n");
+		printf("angle befor = %f\n", vars->player.pa / (M_PI));
+		vars->player.x -= (int)vars->player.d_x;
+		vars->player.y -= (int)vars->player.d_y;
+		printf("angel %f\n",vars->player.pa  / (M_PI));
+		printf("down_d %f %f\n",vars->player.d_x, vars->player.d_y);
+		printf("down %d %d\n",vars->player.x, vars->player.y);
+
+	} 
 	if (keycode == UP || keycode == W)
-		vars->player.y = vars->player.y - vars->game_speed;
+	{
+		printf("\n UP\n--------------------\n");
 
-	// float a = (float)(30 /180) * 3.14159265359 ;
-	// draw_line(vars, a, 10, 0XFFFF00);
-	create_player(vars, vars->player.x, vars->player.y);
+		printf(" before angle = %f\n", vars->player.pa / (M_PI));
+		vars->player.x += (int)vars->player.d_x;
+		vars->player.y += (int)vars->player.d_y;
+		printf("angle = %f\n", vars->player.pa / (M_PI));
+
+		printf("up_d %f %f\n",vars->player.d_x, vars->player.d_y);
+		printf("up %d %d\n",vars->player.x, vars->player.y);
+
+	}
+
+	maps_load(vars, 1);
+	
+
 }
-
-void	check_position(t_vars *vars)
-{
-	int x = vars->player.x /50;
-	int y = vars->player.y / 50;
-	printf("konum x = %d y %d\n", x * 50 , y * 50);
-}
-
 
 int	click_button(int keycode, t_vars *vars)
 {
@@ -61,34 +73,7 @@ int	click_button(int keycode, t_vars *vars)
 		mlx_destroy_window(vars->mlx, vars->win);
         exit(0);
 	}
-	check_position(vars);
-	if (keycode == RIGHT || keycode == D)
-		{
-			if (check_right(vars))
-				result = 0;
-		}
-	else if (keycode == LEFT || keycode == A)
-	{
-		if (check_left(vars))
-			result = 0;
-	}
-
-	else if (keycode == DOWN || keycode == S){
-		if (check_down(vars))
-			result = 0;
-		}
-	else if (keycode == UP || keycode == W)
-	{
-		//printf("burda\n");
-		if (check_up(vars) && result != 0){
-			//printf("girdi\n\n");
-			result = 0;
-		}
-	}
-	else
-		return (0);
-	if (result == 0)
-	change_position(vars, keycode);
+	move(vars, keycode);
 	return (0);
 }
 
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "cub3d.h"
 
-
 void	my_mlx_pixel_put(t_img *img, int x, int y, unsigned int colour)
 {
 	char	*dst;
@@ -12,125 +11,7 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, unsigned int colour)
 	*(unsigned int *)dst = colour;
 }
 
-void	create_win(t_vars *vars, int x, int y)
-{
-	vars->mlx = mlx_init();
-	vars->win = mlx_new_window(vars->mlx, x * 50, y * 50, "CUB3D");
-	vars->background.img_ptr = mlx_new_image(vars->mlx, x * 50, y * 50);
-	vars->background.address = mlx_get_data_addr(vars->background.img_ptr,
-			&vars->background.bits_per_pixel,
-			&vars->background.line_size, &vars->background.endian);
-}
 
-void create_space(t_vars *vars, int a, int b)
-{
-	int x = 0;
-	int y = 0;
-	
-	vars->spaces.img_ptr = mlx_new_image(vars->mlx, 50, 50);
-	vars->spaces.address = mlx_get_data_addr(vars->spaces.img_ptr, &vars->spaces.bits_per_pixel, &vars->spaces.line_size, &vars->spaces.endian);
-
-
-	while (x < 49)
-	{
-		y = 0;
-		while (y < 49)
-		{
-			my_mlx_pixel_put(&vars->spaces, x, y, 0XFFFFFF);
-			y++;
-		}
-		x++;
-	}
-	mlx_put_image_to_window(vars->mlx, vars->win,
-		vars->spaces.img_ptr, a * 50 , b * 50 );
-}
-
-void create_player(t_vars *vars, int player_x, int player_y)
-{
-
-	int x = 0;
-	int y = 0;
-
-	
-	while (x < 15)
-	{
-		y = 0;
-		while (y < 15)
-		{
-			if (((x > 0 && x < 15)&& (y >0 && y <= 5)) || ((x > 5 && x < 10) && (y > 5 && y < 15) ))
-				my_mlx_pixel_put(&vars->player, x, y, 0XE6ADD8);
-			else
-				my_mlx_pixel_put(&vars->player, x, y, 0XFFFFFF);
-			y++;
-		}
-		x++;
-	}
-
-	// float a = (float)30 /180;
-	// draw_line(vars, a *3.14159265359 , 2, 0XE6ADD8);
-
-	mlx_put_image_to_window(vars->mlx, vars->win,
-		vars->player.img_ptr, player_x, player_y);
-}
-void create_walls(t_vars *vars, int wall_x, int wall_y)
-{
-	int x = 0;
-	int y = 0;
-	
-	vars->walls.img_ptr = mlx_new_image(vars->mlx, 50, 50);
-	vars->walls.address = mlx_get_data_addr(vars->walls.img_ptr, &vars->walls.bits_per_pixel, &vars->walls.line_size, &vars->walls.endian);
-
-
-	while (x < 49)
-	{
-		y = 0;
-		while (y < 49)
-		{
-			my_mlx_pixel_put(&vars->walls, x, y, 0X4C1130);
-			y++;
-		}
-		x++;
-	}
-	mlx_put_image_to_window(vars->mlx, vars->win,
-		vars->walls.img_ptr, wall_x * 50 , wall_y * 50 );
-}
-t_vars	*maps_load(t_vars *vars, int where)
-{
-	int	a;
-	int	x;
-
-	x = 0;
-	while (vars->map_info.maps[x])
-	{
-		a = 0;
-		while (vars->map_info.maps[x][a])
-		{
-			if (vars->map_info.maps[x][a] == '0')
-				create_space(vars, a, x);
-			else if (vars->map_info.maps[x][a] == '1')
-			{
-				create_walls(vars, a, x);
-			}
-			else if (vars->map_info.maps[x][a] == 'P' )
-			{
-				create_space(vars, a, x);
-				if (where == 0)
-				{
-					vars->player.y = vars->image_len * x;
-					vars->player.x = vars->image_len * a;
-					vars->player.img_ptr = mlx_new_image(vars->mlx, 15, 15);
-					vars->player.address = mlx_get_data_addr(vars->player.img_ptr, &vars->player.bits_per_pixel, &vars->player.line_size, &vars->player.endian);
-
-					create_player(vars, vars->player.x, vars->player.y);
-					//////////printf("player size %d\n", vars->player.img_width);
-				}
-			}
-			a++;
-		}
-		x++;
-	}
-	return (vars);
-}
 
 
 
@@ -155,21 +36,74 @@ int main()
     char  **map;
 	t_map maps_info;
 
+	// maps1
+	// maps_info.len = 15;
+	// maps_info.line = 5;
+	// map = malloc(sizeof(char**) * maps_info.line + 1);
+	// map[0] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[1] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[2] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[3] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[4] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[5] = malloc(sizeof(char *) * maps_info.len + 1);
+    // map[0] = "111111111111111";
+	// map[1] = "100000000001001";
+	// map[2] = "10000P000001001";
+    // map[3] = "100000000001001";
+	// map[4] = "111111111111111";
+	// map[5] = NULL;
+
+
+	//maps 2
 	maps_info.len = 15;
-	maps_info.line = 5;
-	map = malloc(sizeof(char**) * 6);
-	map[0] = malloc(sizeof(char *) * 13);
-	map[1] = malloc(sizeof(char *) * 13);
-	map[2] = malloc(sizeof(char *) * 13);
-	map[3] = malloc(sizeof(char *) * 13);
-	map[0] = malloc(sizeof(char *) * 13);
+	maps_info.line = 8;
+	map = malloc(sizeof(char**) * maps_info.line + 1);
+	map[0] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[1] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[2] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[3] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[4] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[5] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[6] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[7] = malloc(sizeof(char *) * maps_info.len + 1);
+	map[8] = malloc(sizeof(char *) * maps_info.len + 1);
     map[0] = "111111111111111";
-	map[1] = "100000000001001";
-	map[2] = "10000P000001001";
-    map[3] = "100000000001001";
-	map[4] = "111111111111111";
-	map[5] = NULL;
+	map[1] = "100001000001001";
+	map[2] = "10010P010000101";
+    map[3] = "101001000000001";
+    map[4] = "101000000001001";
+    map[5] = "101000001001001";
+    map[6] = "101001000000001";
+	map[7] = "111111111111111";
+	map[8] = NULL;
+
+
+	//maps3
+	// maps_info.len = 5;
+	// maps_info.line = 10;
+	// map = malloc(sizeof(char**) * maps_info.line + 1);
+	// map[0] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[1] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[2] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[3] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[4] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[5] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[6] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[7] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[8] = malloc(sizeof(char *) * maps_info.len + 1);
+	// map[9] = malloc(sizeof(char *) * maps_info.len + 1);
+    // map[0] = "11111";
+	// map[1] = "10001";
+	// map[2] = "10001";
+	// map[3] = "10001";
+	// map[4] = "10001";
+	// map[5] = "10001";
+	// map[6] = "10001";
+	// map[7] = "10001";
+	// map[8] = "100P1";
+	// map[9] = "11111";
+	// map[10] = NULL;
 	start_draw(map, &maps_info);
 	
 	
-}
+}	
