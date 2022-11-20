2382f83f15454068d589a8cda98f88439dc54e4f
ali-tevfik <tevfikdoner@hotmail.com>
2022-11-20 15:35:59 +0100

    before merge

--- draw_ray.c
+++ draw_ray.c
@@ -1,42 +1,104 @@
 #include "cub3d.h"
 
+int find_y(double y1, double angel)
+{
+    int next_y;
+
+    if (angel > 0.5 * M_PI && angel < 1.5 * M_PI)
+        next_y = (double)(y1 /50 - 0.001) * 50;
+    else
+        next_y = (double)(y1 /50 + 1) * 50;
+    return (next_y);
+}
+
+int find_x(double x1, double angel)
+{
+    int next_x;
+
+    if (angel > M_PI)
+        next_x = ((int)x1 /50 - 0.001) * 50;
+    else
+        next_x = ((int)x1 /50 + 1) * 50;
+    return (next_x + 1);
+}
+
+
 double draw_ray(t_vars *data, double angle, long color)
 {
     double x1;
     double y1;
-	double  i = 0;
-	double len;
-	double oldx;
-	double oldy;
-	oldx = (i * cos(angle)) + data->player.x;
+    double i = 0;
+    double len;
+    double oldx;
+    double oldy;
+    int map_x;
+    int map_y;
+    oldx = (i * cos(angle)) + data->player.x;
     oldy = (i * sin(angle)) + data->player.y;
-	// printf("angel %f\n",angle);
-    while(1)
+    // printf("angel %f\n",angle);
+    while (1)
     {
+        
         x1 = (i * cos(angle)) + data->player.x + 1;
         y1 = (i * sin(angle)) + data->player.y + 1;
-		// printf("x %d y %d ,angel value = %f, y value = %f\n",data->player.x, data->player.y ,data->player.d_x, data->player.d_y);	
-        // printf("x1 %d y1 %d\n",x1,y1);
-		if (x1 < data->map_info.len * 50.0 && x1 > 0  && y1 < data->map_info.line * 50.0 && y1 > 0)
-		{
-			if (check_walls(x1, y1, data))
-			{
-            	my_mlx_pixel_put(&data->two_D, x1 , y1, color);
-				// printf("ray x %d y %d\n",x1,y1);
-			}
-			else
-				break;
-		}
-		else
-		{
-			break;
-		}
-		i += 0.01;
-    }
-	// printf("new x %d - old x %d = %d. and new y %d - old y %d = %d\n",x1,oldx, x1-oldx, y1,oldy, y1-oldy);
-	len = find_ray_len(x1-oldx, y1-oldy);
-	// printf("len %f\n",len);
-	return (len);
+        if (x1 < data->map_info.len * 50.0 && x1 > 0 && y1 < data->map_info.line * 50.0 && y1 > 0)
+        {
+                map_x = find_x(x1, angle);
+                map_y = find_y(y1, angle);
+            if (check_walls(map_x, map_y, data))
+            {
+                while (map_x > map_y && map_x > x1)
+                {
+                    my_mlx_pixel_put(&data->two_D, map_x,  map_y, color);
+                    x1 = (i * cos(angle)) + data->player.x;
+                    y1 = (i * sin(angle)) + data->player.y;
+                    // printf("ustteki while\n");
+                    i += 0.01;
+                }
+               
+                
+                
+            }
+            else
+                break;
+                // else
+                // {
+                //     unsigned int a = ((int)(x1 / 50) + 1) * 50;
+                //     unsigned int b = ((int)(y1 / 50) + 1) * 50;
+
+                //     if (a > b)
+                //     {
+                //         while (a > x1)
+                //         {
+                //         // printf("x1 %f a %d y1 %f b %d\n",x1,a,y1,b);
+                //             x1 = (i * cos(angle)) + data->player.x + 1;
+                //             y1 = (i * sin(angle)) + data->player.y + 1;
+                //             my_mlx_pixel_put(&data->two_D, x1, y1, color);
+                //             i += 0.01;
+                //         }
+                //     }
+                //     else
+                //     {
+                //         while (b > y1)
+                //         {
+                //             x1 = (i * cos(angle)) + data->player.x + 1;
+                //             y1 = (i * sin(angle)) + data->player.y + 1;
+                //             my_mlx_pixel_put(&data->two_D, x1, y1, color);
+                //             i += 0.01;
+                //         }
+                //     }
+                // }
+            }
+            else
+            {
+                break;
+            }
+            i += 0.01;
+        }
+    // printf("new x %d - old x %d = %d. and new y %d - old y %d = %d\n",x1,oldx, x1-oldx, y1,oldy, y1-oldy);
+    len = find_ray_len(x1 - oldx, y1 - oldy);
+    // printf("len %f\n",len);
+    return (len);
 }
 
 void draw_angle(t_vars *data, double angle, int line_lenght, long color)
@@ -44,12 +106,12 @@ void draw_angle(t_vars *data, double angle, int line_lenght, long color)
     double x1;
     double y1;
 
-    for(int i = 0; i < line_lenght; i += 1)
+    for (int i = 0; i < line_lenght; i += 1)
     {
         x1 = (i * cos(angle)) + data->player.x + 1;
         y1 = (i * sin(angle)) + data->player.y + 1;
-		// printf("x %d y %d ,angel value = %f, y value = %f\n",data->player.x, data->player.y ,data->player.d_x, data->player.d_y);	
-        if (x1 < data->map_info.len * 50 && x1 > 0  && y1 < data->map_info.line * 50 && y1 > 0)
+        // printf("x %d y %d ,angel value = %f, y value = %f\n",data->player.x, data->player.y ,data->player.d_x, data->player.d_y);
+        if (x1 < data->map_info.len * 50 && x1 > 0 && y1 < data->map_info.line * 50 && y1 > 0)
             my_mlx_pixel_put(&data->two_D, x1, y1, color);
     }
     // printf("x value = %d, y value = %d\n", x1, y1);
